76941f10f4b33e3b52fcb142327ba3d34676b48c
Halleys123 <arnavadvik2003@gmail.com>
2025-11-07 00:25:25 +0530

    feat: enhance styling and structure for dynamic classes and number setup logic, Numbers have started rendering now

--- packages/editor/index.ts
+++ packages/editor/index.ts
@@ -7,10 +7,11 @@ function setClasses() {
   // Fixed classes
   const div_line = `.vertical-division-line {self-stretch: 1; border-right: 1px solid #999999}`;
 
-  const container_class = `.parent-container { background-color: #323232; width: 100%; height: 100%;display: flex;flex-direction: row; gap: 0;}`;
+  // Dynamic Classes
+  const container_class = `.parent-container { background-color: #323232; width: 100%; height: 100%; overflow: hidden; font-size: 14px; display: flex;flex-direction: row; gap: 0; font-family: monospace;}`;
 
-  const container_class__numbers = `.number-container { self-stretch: 1; min-width: 40px; max-width: 60px; font-size: 14px; text-align: right; }`;
-  const number_span = '.number_span {font-size: 14px; color: white;}';
+  const container_class__numbers = `.number-container { padding: 8px 0; display: flex; flex-direction: column; gap: 4px; align-items: center;justify-content: space-between; self-stretch: 1; min-width: 40px; max-width: 60px; font-size: 14px; text-align: right; }`;
+  const number_span = '.number_span { color: white; }';
   const dummy_number_span = `.dummy_number_span {opacity: 0; position: absolute; top: 0; left: 0;}`;
 
   const container_class__code = `.code-container { self-stretch: 1; flex: 1; font-size: 14px }`;
@@ -36,6 +37,7 @@ function createContainer(parentId: string) {
   number_div.classList.add('number-container');
 
   const dummy_span = document.createElement('span');
+  dummy_span.innerText += '1';
   dummy_span.classList.add('number_span', 'dummy_number_span');
 
   number_div.insertAdjacentElement('beforeend', dummy_span);
@@ -53,21 +55,47 @@ function createContainer(parentId: string) {
   console.log(div);
 
   document.getElementById(parentId)?.insertAdjacentElement('beforeend', div);
+
+  setupNumbers(number_div, dummy_span);
 }
 
-function setupNumbers(numbersDiv: HTMLDivElement) {
-  // Get dimensions using dummy span
-  const dummy_span = document.querySelector('dummy_number_span');
-
-  if (!dummy_span) {
+function setupNumbers(numbersDiv: HTMLDivElement, dummySpan: HTMLSpanElement) {
+  if (!dummySpan || !numbersDiv) {
     console.error(
       'There was some error initializing the editor dummy span not found'
     );
+    console.info(`Parent: ${!!numbersDiv} and Dummy Span: ${!!dummySpan}`);
     return;
   }
 
-  const height: string = getComputedStyle(dummy_span).height; // pixels
-  const width: string = getComputedStyle(dummy_span).width;
+  const spanStyles = getComputedStyle(dummySpan);
+  const numberContainerStylers = getComputedStyle(numbersDiv);
+
+  const heightStr = spanStyles.height;
+
+  const containerHeightStr = numberContainerStylers.height;
+  const containerPaddingYStr = numberContainerStylers.paddingTop;
+  const containerGapStr = numberContainerStylers.gap;
+
+  const height: number = Math.round(parseFloat(heightStr) || 1);
+  const container_height: number = Math.round(
+    parseFloat(containerHeightStr) || 1
+  );
+  const container_padding_y = Math.round(parseFloat(containerPaddingYStr) || 0);
+  const container_gap = Math.round(parseFloat(containerGapStr) || 0);
+
+  const maxLines: number = Math.round(
+    (container_height - container_padding_y) / (height + container_gap)
+  );
+  console.log(maxLines);
+
+  for (let i = 1; i <= maxLines; i += 1) {
+    const span = document.createElement('span');
+    span.innerText += i.toString();
+    span.classList.add('number_span');
+
+    numbersDiv.insertAdjacentElement('beforeend', span);
+  }
 
   const number_span = document.createElement('span');
 }
